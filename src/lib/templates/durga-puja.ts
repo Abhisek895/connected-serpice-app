0df347948d282d039c17f4317f0556b93f6928d8
Abhisek895 <abhisek.sarkar@retechprime.com>
2026-09-20 23:47:41 +0530

    feat: add Durga Puja template and related food images

--- src/lib/templates/durga-puja.ts
+++ src/lib/templates/durga-puja.ts
@@ -123,9 +123,9 @@ export const DEFAULT_FOOD_OPTIONS = [
   { id: "phuchka", name: "Phuchka", icon: "🧆", desc: "Crispy, tangy & irresistible", image: "/images/food/phuchka.jpg" },
   { id: "momos", name: "Momos", icon: "🥟", desc: "Steaming hot with spicy chutney", image: "/images/food/momos.jpg" },
   { id: "roll", name: "Kathi Roll", icon: "🌯", desc: "Classic festival street food", image: "/images/food/kathi_roll.jpg" },
-  { id: "biryani", name: "Biryani", icon: "🥘", desc: "Fragrant, royal & comforting", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/%22Hyderabadi_Dum_Biryani%22.jpg/800px-%22Hyderabadi_Dum_Biryani%22.jpg" },
-  { id: "sweet", name: "Rosogolla", icon: "⚪", desc: "The classic sweet to end the day", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Rasgulla_from_Bikaneer_Sweets.jpg/800px-Rasgulla_from_Bikaneer_Sweets.jpg" },
-  { id: "you-choose", name: "You choose", icon: "❤️", desc: "Surprise me with your pick", image: "/images/food/you_choose.png" },
+  { id: "biryani", name: "Biryani", icon: "🥘", desc: "Fragrant, royal & comforting", image: "/images/food/biryani.jpg" },
+  { id: "sweet", name: "Rosogolla", icon: "⚪", desc: "The classic sweet to end the day", image: "/images/food/rosogolla.jpg" },
+  { id: "you-choose", name: "You choose", icon: "❤️", desc: "Surprise me with your pick", image: "/images/food/you_choose.jpg" },
 ];
 
 export const DEFAULT_ACTIVITIES = [
